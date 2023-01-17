9d27b2cd2c7725b7e57b8c5f647486af31f18bc4
Nik Kremenskov <nikremenskovs@gmail.com>
2023-01-17 18:03:06 +0000

    Main section content #3
    -add text content with scroll animation to main section

--- src/components/homepage/HeroSection.vue
+++ src/components/homepage/HeroSection.vue
@@ -1,7 +1,21 @@
 <template>
-    <div class="img-container" ref="imgContainer">
-        <div class="background" ref="background" :style="{ transform: backgroundTransform }"></div>
-        <div class="foreground" ref="foreground" :style="{ transform: foregroundTransform }"></div>
+    <div class="img-container">
+        <div class="background" :style="{ transform: backgroundTransform }"></div>
+        <div class="overlay" :style="{ opacity: overlayOpacity }"></div>
+        <div class="foreground" :style="{ transform: foregroundTransform }"></div>
+    </div>
+    <div class="hero-title" :style="{ opacity: heroTitleOpacity }">
+        <h1>minerals</h1>
+        <h2>and more</h2>
+    </div>
+    <div class="hero-info" :style="{ opacity: heroInfoOpacity }">
+        <h2>why us?</h2>
+        <p :style="{ transform: heroInfoParaTransform }">You should buy minerals because they are beautiful, they appeal
+            to
+            you, each one is unique, natural, and not
+            man-made. Do not buy minerals as investments - unless you have 20 years to wait for a return on your
+            investment. And definitely do not believe a mineral dealer if he advises you to invest in minerals - the
+            only person that will realize a profit is the dealer.</p>
     </div>
 </template>
 
@@ -9,10 +23,10 @@
 import { useScroll } from '@vueuse/core'
 export default {
     setup() {
-        const scroll = useScroll(window)
+        const windowScroll = useScroll(window)
 
         return {
-            scrY: scroll.y,
+            scrY: windowScroll.y,
             maxBackgroundSize: 114,
         }
     },
@@ -25,12 +39,28 @@ export default {
         },
         foregroundTransform() {
             return `scale3d(${(100 + this.backgroundSize) / 100}, ${(100 + this.backgroundSize) / 100}, 1)`
+        },
+        heroTitleOpacity() {
+            return `${1 - this.scrY / 10}`
+        },
+        heroInfoOpacity() {
+            return `${0 + this.scrY / 1000}`
+        },
+        overlayOpacity() {
+            return `${0 + this.scrY / 2500}`
+        },
+        heroInfoParaTransform() {
+            return `translate3d(-50%, -${100 + this.scrY / 20}px, 0px)`
         }
     }
 }
 </script>
 
 <style scoped>
+* {
+    font-family: sans-serif;
+}
+
 .img-container {
     height: 2000px;
 }
@@ -53,5 +83,46 @@ export default {
     width: 100%;
     height: 100vh;
     scale: 1.5;
+}
+
+.hero-title {
+    position: relative;
+    text-transform: uppercase;
+    text-align: center;
+    color: white;
+    font-size: 2rem;
+    position: fixed;
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%, -50%);
+    line-height: 75%;
+}
+
+.hero-info {
+    opacity: 0;
+    position: fixed;
+    left: 50%;
+    top: 50%;
+    text-align: center;
+    color: white;
+}
+
+.hero-info h2 {
+    text-transform: capitalize;
+    font-size: 3rem;
+    transform: translate(-50%, -250%);
+}
+
+.hero-info p {
+    font-size: 1rem;
+    padding: 0 0.1rem;
+}
+
+.overlay {
+    position: fixed;
+    width: 100%;
+    height: 100vh;
+    background-color: black;
+    opacity: 0;
 }
 </style>
