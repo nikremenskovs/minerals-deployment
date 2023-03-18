d9f971db3a0fc0f81003e80f205a819e0701fd9c
Nik Kremenskov <nikremenskovs@gmail.com>
2023-03-18 14:51:18 +0000

    deploy

--- src/main.ts
+++ src/main.ts
@@ -8,14 +8,24 @@ import router from "./router";
 import ResponsiveImage from "./components/shared/ResponsiveImage.vue";
 import NotFound from "./components/shared/NotFound.vue";
 
-await createSquidexService();
+// await createSquidexService();
+// const app = createApp(App);
+// app.use(createPinia());
+// app.use(router);
 
-const app = createApp(App);
+// app.mount("#app");
 
-app.use(createPinia());
-app.use(router);
+// app.component("ResponsiveImage", ResponsiveImage);
+// app.component("NotFound", NotFound);
 
-app.mount("#app");
+(async () => {
+  await createSquidexService();
+  const app = createApp(App);
+  app.use(createPinia());
+  app.use(router);
 
-app.component("ResponsiveImage", ResponsiveImage);
-app.component("NotFound", NotFound);
+  app.mount("#app");
+
+  app.component("ResponsiveImage", ResponsiveImage);
+  app.component("NotFound", NotFound);
+})();
